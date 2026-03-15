a93d4c0110a89899347f341eda94fd0bfb5ba3d1
Shannon <190230671+5h6nn0n@users.noreply.github.com>
2026-03-15 19:27:17 -0400

    Add files via upload

---
+++ editEvent.php
@@ -0,0 +1,47 @@
+<?php
+require_once './include/db_connect.php';
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+$user_id = $_SESSION['user']['user_id'];
+
+$eventId = filter_input(INPUT_POST, 'event_id', FILTER_VALIDATE_INT);
+$editTitle  = filter_input(INPUT_POST, 'title');
+$editDate   = filter_input(INPUT_POST, 'edit_date');
+$editLocation = filter_input(INPUT_POST, 'location');
+$editDescription  = filter_input(INPUT_POST, 'description');
+$saveChanges = filter_input(INPUT_POST, 'saveChanges');
+
+if 
+
+($editTitle == null || $editDate == null || 
+		$editLocation == null || $editDescription == null) 
+{
+	header('Location: calendar.php?error=1');
+}
+else 
+{ 
+
+//update query
+$updateEvent = 'UPDATE calendarEvent
+		 SET event_title = :editTitle, 
+		 event_desc =:editDescription, 	 
+		 event_location =:editLocation, 
+		 event_date = :editDate, 
+		 updated_at = NOW(),
+		 updated_by = :updated_by
+		 WHERE event_id =:eventId';
+$statement = $db->prepare($updateEvent);
+$statement->bindValue(':eventId', $eventId);
+$statement->bindValue(':editTitle', $editTitle);
+$statement->bindValue(':editDescription', $editDescription);
+$statement->bindValue(':editLocation', $editLocation);
+$statement->bindValue(':editDate', $editDate);
+$statement->bindValue(':updated_by', $user_id);
+$statement->execute();
+$statement->closeCursor();
+ 
+	header('Location: calendar.php?success=1');	
+}
+?>
