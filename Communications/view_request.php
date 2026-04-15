d72d70520e096b43badbcc5de0ba0ef9be8495b6
Shannon <190230671+5h6nn0n@users.noreply.github.com>
2026-04-15 13:51:51 -0400

    Refactor view_request.php for improved structure

--- Communications/view_request.php
+++ Communications/view_request.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
-require_once __DIR__ . "/include/db_connect.php";
-require_once __DIR__ . "/include/auth.php";
+require_once __DIR__ . "/../include/db_connect.php";
+require_once __DIR__ . "/../include/auth.php";
 
 require_role(["President"]);
 global $db;
