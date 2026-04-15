a1872f1d08c56639feb05e1a182cf5b34df798e9
Shannon <190230671+5h6nn0n@users.noreply.github.com>
2026-04-15 13:53:09 -0400

    Update thank_you.php to use relative paths

--- Communications/thank_you.php
+++ Communications/thank_you.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['submitted'])) {
-    header("Location: loginpages/login.php");
+    header("Location: ../login.php");
     exit;
 }
 unset($_SESSION['submitted']);
@@ -11,7 +11,7 @@ unset($_SESSION['submitted']);
 <head>
   <meta charset="UTF-8">
   <title>Submission Successful</title>
-  <link rel="stylesheet" type="text/css" href="loginpages/style.css" />
+  <link rel="stylesheet" type="text/css" href="../style.css" />
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
 </head>
 <body>
@@ -19,7 +19,7 @@ unset($_SESSION['submitted']);
         <h1>Submission Successful 🎉</h1>
         <p>Thank you for reaching out! Your request has been received.</p>
         <br>
-        <a href="loginpages/index.php" style="color: #8b6f47; text-decoration: none; font-weight: 600;">Back to Home</a>
+        <a href="../index.php" style="color: #8b6f47; text-decoration: none; font-weight: 600;">Back to Home</a>
     </div>
 </body>
 </html>
