5ff1055517e4be65a6e82d8cf1bac735596875e8
Shannon <190230671+5h6nn0n@users.noreply.github.com>
2026-04-16 19:58:17 -0400

    Add comment block to config.php for clarity
    Added a comment block describing the usage of the configuration settings in various parts of the application.

--- include/config.php
+++ include/config.php
@@ -1,4 +1,9 @@
 <?php
+/* Appears in:
+   newEvent, president_requests, record_attendance, headdepartnementSummary, 
+   memberSummary, manage_roles, viewUser, newUser, and userInfo
+*/
+
 $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
 $host = $_SERVER['HTTP_HOST'];
 
