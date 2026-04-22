f2fa2b7d0ee3242a8e7f4d7666e3b0584bd68fec
Shannon <190230671+5h6nn0n@users.noreply.github.com>
2026-04-22 17:46:58 -0400

    Refactor BASE_URL definition and project root handling

--- include/config.php
+++ include/config.php
@@ -2,18 +2,20 @@
 /* Appears in:
    newEvent, president_requests, record_attendance, headdepartnementSummary, 
    memberSummary, manage_roles, viewUser, newUser, and userInfo
+
+   also used in surveyHub
 */
 
 $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
 $host = $_SERVER['HTTP_HOST'];
 
-// Get the directory of the current script
-$basePath = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
+// Get first directory (project root)
+$pathParts = explode('/', trim($_SERVER['SCRIPT_NAME'], '/'));
+$projectFolder = $pathParts[0] ?? '';
 
-// If it's root, avoid double slashes
-$basePath = $basePath === '/' ? '' : $basePath;
+$basePath = $projectFolder ? '/' . $projectFolder . '/' : '/';
 
-define('BASE_URL', $protocol . '://' . $host . $basePath . '/');
+define('BASE_URL', $protocol . '://' . $host . $basePath);
 
 
 ?>
