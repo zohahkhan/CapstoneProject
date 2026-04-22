8b3718c43ac306e7f825d968fec01ea13a4c353f
Shannon <190230671+5h6nn0n@users.noreply.github.com>
2026-04-22 17:04:22 -0400

    Update BASE_URL definition in config.php
    Refactor base path calculation to avoid double slashes.

--- include/config.php
+++ include/config.php
@@ -7,10 +7,13 @@
 $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
 $host = $_SERVER['HTTP_HOST'];
 
-$basePath = dirname(dirname($_SERVER['SCRIPT_NAME']));
-$basePath = '/' . trim($basePath, '/\\');
+// Get the directory of the current script
+$basePath = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
 
-define('BASE_URL', $protocol . '://' . $host . $basePath);
+// If it's root, avoid double slashes
+$basePath = $basePath === '/' ? '' : $basePath;
+
+define('BASE_URL', $protocol . '://' . $host . $basePath . '/');
 
 
 ?>
